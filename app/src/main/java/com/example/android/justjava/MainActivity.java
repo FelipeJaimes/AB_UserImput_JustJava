5f3a3a6130392ddd870b9c1ef31a57eee0b4378f
Felipe <felipejg8611@gmail.com>
2018-01-12 22:24:16 -0500

    Added menu and extra functionality.

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -14,50 +14,105 @@ import java.text.NumberFormat;
  */
 public class MainActivity extends AppCompatActivity {
 
-    TextView textViewQuantity;
+    TextView textViewQuantityLate;
+    TextView textViewQuantityMoca;
+    TextView textViewQuantityCappuccino;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textViewQuantity = findViewById(R.id.quantity_text_view);
+        textViewQuantityLate = findViewById(R.id.quantity_text_view_late);
+        textViewQuantityMoca = findViewById(R.id.quantity_text_view_moca);
+        textViewQuantityCappuccino = findViewById(R.id.quantity_text_view_cappuccino);
     }
 
 
-    public void increment(View view) {
-        String stringQuantity = textViewQuantity.getText().toString();
+    public void incrementLate(View view) {
+        String stringQuantity = textViewQuantityLate.getText().toString();
         int quantity = Integer.parseInt(stringQuantity);
         quantity++;
-        display(quantity);
+        display(quantity, R.id.quantity_text_view_late);
     }
 
-    public void decrement(View view) {
-        String stringQuantity = textViewQuantity.getText().toString();
+    public void decrementLate(View view) {
+        String stringQuantity = textViewQuantityLate.getText().toString();
         int quantity = Integer.parseInt(stringQuantity);
         if (quantity != 0) {
             quantity--;
-            display(quantity);
+            display(quantity, R.id.quantity_text_view_late);
         }
-        display(quantity);
+        display(quantity, R.id.quantity_text_view_late);
+    }
+
+    public void incrementMoca(View view) {
+        String stringQuantity = textViewQuantityMoca.getText().toString();
+        int quantity = Integer.parseInt(stringQuantity);
+        quantity++;
+        display(quantity, R.id.quantity_text_view_moca);
+    }
+
+    public void decrementMoca(View view) {
+        String stringQuantity = textViewQuantityMoca.getText().toString();
+        int quantity = Integer.parseInt(stringQuantity);
+        if (quantity != 0) {
+            quantity--;
+            display(quantity, R.id.quantity_text_view_moca);
+        }
+        display(quantity, R.id.quantity_text_view_moca);
+    }
+
+    public void incrementCappuccino(View view) {
+        String stringQuantity = textViewQuantityCappuccino.getText().toString();
+        int quantity = Integer.parseInt(stringQuantity);
+        quantity++;
+        display(quantity, R.id.quantity_text_view_cappuccino);
+    }
+
+    public void decrementCappuccino(View view) {
+        String stringQuantity = textViewQuantityCappuccino.getText().toString();
+        int quantity = Integer.parseInt(stringQuantity);
+        if (quantity != 0) {
+            quantity--;
+            display(quantity, R.id.quantity_text_view_cappuccino);
+        }
+        display(quantity, R.id.quantity_text_view_cappuccino);
     }
 
     /**
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        String stringQuantity = textViewQuantity.getText().toString();
-        int quantity = Integer.parseInt(stringQuantity);
-        display(quantity);
-        displayPrice(quantity * 5);
-        String priceMessage = "Thank you for buying " + quantity +" cups of coffee!";
+        String stringQuantityLate = textViewQuantityLate.getText().toString();
+        String stringQuantityMoca = textViewQuantityMoca.getText().toString();
+        String stringQuantityCappuccino = textViewQuantityCappuccino.getText().toString();
+        int quantityLate = Integer.parseInt(stringQuantityLate);
+        int quantityMoca = Integer.parseInt(stringQuantityMoca);
+        int quantityCapuccino = Integer.parseInt(stringQuantityCappuccino);
+        displayPrice(quantityLate * 5 + quantityMoca * 5 + quantityCapuccino * 5);
+        String priceMessage = "Thank you for buying ";
+        if (quantityLate == 0 && quantityMoca == 0 && quantityCapuccino == 0) {
+            priceMessage = priceMessage + "nothing cheap fuck!";
+            displayMessage(priceMessage);
+        }
+        if (quantityLate != 0) {
+            priceMessage = priceMessage + quantityLate + " cups of Late. ";
+        }
+        if (quantityLate != 0) {
+            priceMessage = priceMessage + quantityMoca + " cups of Moca. ";
+        }
+        if (quantityLate != 0) {
+            priceMessage = priceMessage + quantityCapuccino + " cups of Cappuccino. ";
+        }
         displayMessage(priceMessage);
+
     }
 
     /**
      * This method displays the given quantity value on the screen.
      */
-    private void display(int number) {
-        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+    private void display(int number, int id) {
+        TextView quantityTextView = (TextView) findViewById(id);
         quantityTextView.setText("" + number);
     }
 
